a124e43cf077914b1245153ed0fda87c8b55b19e
Elizabeth Mary <elizabethjems@gmail.com>
2021-08-31 15:00:36 +0530

    Upload from master

---
+++ src/basic/TestConflict.java
@@ -0,0 +1,10 @@
+package basic;
+
+public class TestConflict {
+
+	public static void main(String[] args) {
+		System.out.println("Good Evening");
+		System.out.println("Welcome to Bangalore");
+	}
+
+}
